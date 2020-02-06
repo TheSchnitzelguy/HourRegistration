8264a38a2bb5b8643805f6a3c65e4766d577627e
Valentijn Hulst <valentijn169@live.nl>
2020-02-06 22:42:08 +0100

    finished GUI

---
+++ widget.cpp
@@ -0,0 +1,37 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+
+    qDebug() << "submit button pressed!";
+    //QMessageBox::information(this, "Message", "Submit", QMessageBox::Ok);
+
+    QMessageBox::StandardButton button;
+    button = QMessageBox::question(this, "Urenregistratie", "Ingevoerde uren bij maand rekenen?", QMessageBox::Yes | QMessageBox::No);
+
+
+    if(button == QMessageBox::Yes)
+    {
+        qDebug() << "Hours pushed to database";
+    }
+    else
+    {
+        qDebug() << "Hour mutation cancelled";
+    }
+}
